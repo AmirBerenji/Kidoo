8274fd5b057de39f886220c4387e9e6010ac92b8
AmirBerenji <a.berenji62@gmail.com>
2025-09-19 08:44:19 +0400

    Change

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::prefix('user')->group(function () {
         Route::get('profile', [AuthController::class, 'user']); // changed from 'user/user' to 'user/profile'
         Route::post('logout', [AuthController::class, 'logout']);
         Route::put('update', [AuthController::class, 'update']);
-        Route::put('/user/photo',[AuthController::class, 'updatePhoto']);
+        Route::put('photo',[AuthController::class, 'updatePhoto']);
     });
 });
 
